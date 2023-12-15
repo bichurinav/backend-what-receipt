90f8a5a131b131a9e0732610dc3753cd04f903c2
bichurinav <bichurin.overclock1ng@gmail.com>
2023-12-16 01:09:59 +0300

    update Receipt

--- src/controllers/Receipt/index.ts
+++ src/controllers/Receipt/index.ts
@@ -1,6 +1,5 @@
 import { Response, Request } from "express";
-import fs from "fs/promises";
-import paths from "../../paths";
+import { getCollectionDB } from "../../utils/helpers";
 import { IReceipt, IReceiptMatched } from "./types";
 import { getMatchProducts } from "./helpers";
 
@@ -13,73 +12,73 @@ const categories: string[] = [
 ];
 
 export default new (class ReceiptController {
-  getReceipts(req: Request, res: Response) {
-    const { compositions, mode } = req.query;
-    const { category } = req.params;
+  async getReceipts(req: Request, res: Response) {
+    try {
+      const { compositions, mode } = req.query;
+      const { category } = req.params;
 
-    if (!categories.includes(category)) {
-      res.status(400).json({
-        message: `Данная категория не существует: ${category}`,
-        data: [],
-      });
-      return;
-    }
+      if (!categories.includes(category)) {
+        res.status(400).json({
+          message: `Данная категория не существует: ${category}`,
+          data: [],
+        });
+        return;
+      }
 
-    const userCompositions = !Array.isArray(compositions)
-      ? [compositions]
-      : compositions;
+      const userCompositions = !Array.isArray(compositions)
+        ? [compositions]
+        : compositions;
 
-    if (Boolean(userCompositions[0])) {
-      fs.readFile(paths.db_item(category), {
-        encoding: "utf-8",
-      })
-        .then((jsonData) => {
-          const receipts: IReceipt[] = JSON.parse(jsonData);
+      if (Boolean(userCompositions[0])) {
+        const receipts = await getCollectionDB(category);
 
-          const matchedReceipts: IReceiptMatched[] = [];
-
-          receipts.forEach((receipt: IReceipt) => {
-            const { matchedCount, matchedProducts } = getMatchProducts(
-              userCompositions as string[],
-              receipt.compositions
-            );
-
-            const addReceipt = () => {
-              matchedReceipts.push({
-                ...receipt,
-                matchedCount,
-                matchedProducts,
-              });
-            };
-
-            if (mode === "strict") {
-              if (
-                matchedCount !== 0 &&
-                receipt.compositions.length - matchedCount < 3
-              )
-                addReceipt();
-            } else {
-              if (matchedCount !== 0 && matchedCount > 0) addReceipt();
-            }
-          });
-
-          const data = matchedReceipts
-            .sort((a, b) => b.matchedCount - a.matchedCount)
-            .splice(0, 60);
-
-          res.status(200).json({ data, total: data.length });
-        })
-        .catch((err) => {
+        if (!receipts) {
           res.status(500).json({
-            message: err,
+            message: `Ошибка в чтении базы ${category}`,
             data: [],
           });
+          return;
+        }
+
+        const matchedReceipts: IReceiptMatched[] = [];
+
+        receipts.forEach((receipt: IReceipt) => {
+          const { matchedCount, matchedProducts } = getMatchProducts(
+            userCompositions as string[],
+            receipt.compositions
+          );
+
+          const addReceipt = () => {
+            matchedReceipts.push({
+              ...receipt,
+              matchedCount,
+              matchedProducts,
+            });
+          };
+
+          if (mode === "strict") {
+            if (
+              matchedCount !== 0 &&
+              receipt.compositions.length - matchedCount < 3
+            )
+              addReceipt();
+          } else {
+            if (matchedCount !== 0 && matchedCount > 0) addReceipt();
+          }
         });
-    } else {
-      res.status(400).json({
-        message: "Query params не массив string[]",
+
+        const data = matchedReceipts
+          .sort((a, b) => b.matchedCount - a.matchedCount)
+          .splice(0, 60);
+
+        res.status(200).json({ data, total: data.length });
+      }
+    } catch (err) {
+      res.status(500).json({
+        message: err,
         data: [],
       });
+      console.error(err);
     }
   }
 })();
